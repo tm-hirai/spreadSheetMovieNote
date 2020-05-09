e0e3eb8607b6ebb83b2093bd500413a071faab2b
tomoki-hirai <thirai@mikilab.doshisha.c.jp>
2020-05-09 23:05:16 +0900

    user.js削除

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var oauthRouter = require('./routes/oauth');
 var searchRouter = require('./routes/search');
 
@@ -22,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/oauth', oauthRouter);
 app.use('/search', searchRouter);
 
