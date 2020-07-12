b839abf82e496026e67cbd7012473fd6313b5af3
tomoki-hirai <thirai@mikilab.doshisha.c.jp>
2020-07-12 20:56:07 +0900

    shett

--- routes/sheet.js
+++ routes/sheet.js
@@ -7,5 +7,10 @@ router.get('/', function (req, res, next) {
   const title = 'Home';
   res.render("index", {title: title,max: 5,message:[1,2,3,4,5]});
 });
+router.post('/', function (req, res, next) {
+//   const title = 'Home';
+//   res.render("index", {title: title,max: 5,message:[1,2,3,4,5]});
+    res.send(req.body);
+});
 
 module.exports = router;
